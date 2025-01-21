c490e1d9ad87030803d50b166d506f1574792e11
pquadro <116500148+pquadro@users.noreply.github.com>
2025-01-22 00:22:35 +0100

    Add files via upload

---
+++ src/main/java/org/synapse/HelloWorldApi.java
@@ -0,0 +1,7 @@
+package org.synapse;
+
+public class HelloWorldApi {
+    public static void printHelloWorld() {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/test/java/Main.java
@@ -0,0 +1,7 @@
+import org.synapse.HelloWorldApi;
+
+public class Main {
+    public static void main(String[] args) {
+        HelloWorldApi.printHelloWorld();
+    }
+}
